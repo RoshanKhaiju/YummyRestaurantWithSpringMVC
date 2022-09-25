4386da220fd4ccb3fea79ca6068e352a670029da
RoshanKhaiju <khaijur5@gmail.com>
2022-09-25 15:04:31 +0545

    first commit

---
+++ src/main/java/com/ecommerce/ECommerceWithSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWithSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWithSpringMvcApplication.class, args);
+	}
+
+}
